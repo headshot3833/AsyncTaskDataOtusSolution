56e699cc5ed22dcf5c5e8e74e67be0434e58ae77
head <headshot_1998@mail.ru>
2024-09-07 02:08:15 +0400

    Добавьте файлы проекта.

---
+++ AsyncTaskDataOtusSolution/Program.cs
@@ -0,0 +1,29 @@
+﻿using AsyncTaskDataOtusSolution.Services;
+using System.Text;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        UserReadFile userReadFile = new UserReadFile();
+        Dictionary<string, int> fileSpaceCounts = new Dictionary<string, int>();
+        var directoryPath = Console.ReadLine();
+        var files = Directory.GetFiles(directoryPath);
+
+        var tasks = files.Select(async file =>
+        {
+            int spaceCount = await userReadFile.ReadFileAsync(file);
+            return (file, spaceCount);
+        });
+        var results = await Task.WhenAll(tasks);
+        foreach (var result in results)
+        {
+            fileSpaceCounts[result.file] = result.spaceCount;
+        }
+
+        foreach(var kvp in fileSpaceCounts)
+        {
+            Console.WriteLine($"{kvp.Key}, пробелов: {kvp.Value}");
+        }
+    }
+}
---
+++ AsyncTaskDataOtusSolution/Services/UserReadFile.cs
@@ -0,0 +1,26 @@
+﻿using AsyncTaskDataOtusSolution.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncTaskDataOtusSolution.Services
+{
+    public class UserReadFile : IUserReadFiles
+    {
+        public async Task<int> ReadFileAsync(string path)
+        {
+            
+            if(path == null)
+            {
+                throw new ArgumentNullException("нужно ввести путь");
+            }
+            string content = await File.ReadAllTextAsync(path);
+            int spaceCount = content.Count(c => c == ' ');
+
+            return spaceCount;
+
+        }
+    }
+}
